2851500d8a96e8ae92503e3d847bd5a9058ceca4
Ralitsa Djurkova <ralitsadj@gmail.com>
2021-01-14 06:58:13 +0200

    Added tasks 06-07 from the exercises.

--- 06. SongsQueue/Program.cs
+++ 06. SongsQueue/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06._SongsQueue
 {
@@ -6,7 +8,42 @@ namespace _06._SongsQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] songsName = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Queue<string> songs = new Queue<string>(songsName);
+            string command = Console.ReadLine();
+
+            while (songs.Count > 0)
+            {
+               
+                if (command.Contains("Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if(command.Contains("Add"))
+                {
+                    var songToAdd = command.Split("Add ");
+                    if (!songs.Contains(songToAdd[1]))
+                    {
+                        songs.Enqueue(songToAdd[1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songToAdd[1]} is already contained!");
+                    }
+                }
+                else if(command.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ",songs));
+                }
+               
+                command = Console.ReadLine();
+            }
+            if (songs.Count == 0)
+            {
+                Console.WriteLine("No more songs!");
+
+            }
+
         }
     }
 }
---
+++ 07.TruckTour/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsOfPomps = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+
+            for (int i = 0; i < numsOfPomps; i++)
+            {
+                string input = Console.ReadLine();
+                input += $" {i}";
+                queue.Enqueue(input);
+            }
+            int totalfuel = 0;
+            for (int i = 0; i < numsOfPomps; i++)
+            {
+                string currentInfo = queue.Dequeue();
+                var info = currentInfo.Split().Select(int.Parse).ToArray();
+
+                var fuel = info[0];
+                var distance = info[1];
+                totalfuel += fuel;
+
+                if(totalfuel >= distance)
+                {
+                    totalfuel -= distance;
+                }
+                else
+                {
+                    totalfuel = 0;
+                    i = -1;
+                }
+
+                queue.Enqueue(currentInfo);
+            }
+            var firstElement = queue.Dequeue().Split().ToArray();
+            Console.WriteLine(firstElement[2]);
+        }
+    }
+}
